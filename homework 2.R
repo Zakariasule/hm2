2c29b2311ee75ce7a9b0b455226b4491f6fdc403
Zakariasule <112271508+Zakariasule@users.noreply.github.com>
2022-09-09 03:07:55 -0400

    Add files via upload

---
+++ homework 2.R
@@ -0,0 +1,22 @@
+homework 2
+Zakaria Sule
+
+Group 2
+Names: Zack, Victoria and Mario
+
+In light of lab #1 :
+
+#Possible Protocol 1 (PP1):
+  The dice will be fair when we get any number either than 6 after rolling once, in our case we chose 1, if the dice were fair{we roll other numbers either than 6} then the probability of judging the dice to be unfair is E/S, E[1,2,3,4,5] and s[6]= 5/6= 83%
+  oppositely the probability of the dice been fair is 1/6= 16%
+  
+#PP2:
+  If the dice is rolled 20 times the likely number of 6 resulting will be =1/6*20=5, from this its unsusual to get 5 more. 
+  If the dice were fair after 20 rolls,the chance it could be judged as unfair in percentage wise will be 100% since there are equal chances of rolling other numbers.
+  
+#PP3:
+  In rolling 100 times, every single roll might come to 6 or might come to any of the edge but there is definately some level of confidence attatched to each outcome.
+  After doing Lab 1 my group concluded that rolling the dice  at minimum 36 times is the best way to evaluate on if the dice is fair or not fair. Giving the dice 36 rolls allows the dice  an equal amount of chance of each side being rolled.
+  After rolling the dice 36 times, and evaluating how many times the dice rolled certain numbers. I still agree that 36 times is a good number to have as a minimum on figuring out if the dice is fair. 
+  
+  
